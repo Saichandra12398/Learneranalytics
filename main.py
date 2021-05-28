fcf206f5fb6cd9b04ae364f62b2bd7b4bf76c5fc
santosh1987 <83802973+santosh1987@users.noreply.github.com>
2021-05-28 12:04:15 +0530

    Divsion Function Added

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 
 def subtract(a,b):
   return a-b
-
+def division(a, b):
+  return a/b
 def addition(a, b):
   return a + b
 def __main__():
   print("Addition of 1 and 2 is ", addition(1,2))
+  print("Division of 1 and 2 is ", division(1,2))
   print(subtract(1,2))
   pass
 
